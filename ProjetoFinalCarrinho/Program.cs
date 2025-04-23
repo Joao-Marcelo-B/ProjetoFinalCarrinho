1b89bb0359ea607e4ad6b5331fd2e9dd047bfde6
RafaelSilveira12 <RAFAEL.SILVEIRA@ALUNO.UNIFENAS.BR>
2025-04-23 15:42:21 -0300

    carinho funcional

--- ProjetoFinalCarrinho/Program.cs
+++ ProjetoFinalCarrinho/Program.cs
@@ -6,6 +6,10 @@ namespace ProjetoFinalCarrinho
 {
     internal class Program
     {
+        static float tx = 0.3f;
+        static float ty = 0.3f;
+        const float PI = 3.14f;
+
         static void Main(string[] args)
         {
             Glut.glutInit();
@@ -14,8 +18,10 @@ namespace ProjetoFinalCarrinho
             Glut.glutCreateWindow("Carrinho");
             inicializa();
             Glut.glutDisplayFunc(desenha);
+            Glut.glutSpecialFunc(TeclasEspeciais);
             Glut.glutMainLoop();
         }
+
         static void inicializa()
         {
             Gl.glClearColor(0, 0, 0, 0);
@@ -29,115 +35,32 @@ namespace ProjetoFinalCarrinho
             fundo();
             pista();
             detalhesPista();
+            desenhaCarrinho();
         }
 
-        static void circunferencia()
+        public static void TeclasEspeciais(int key, int x, int y)
         {
-            float PI = (float)Math.PI;
-            float raio, x, y, pontos;
-            raio = 0.2f;
-            pontos = (2 * PI) / 100;
-            Gl.glColor3f(0.0f, 0.0f, 0.0f);
-            Gl.glLineWidth(15);
-            Gl.glBegin(Gl.GL_TRIANGLE_FAN);
-            for (float angulo = 0.0f; angulo <= 2 * PI; angulo += pontos)
-            {
-                x = (float)(raio * Math.Cos(angulo) + 0.5);
-                y = (float)(raio * Math.Sin(angulo) + 0.8);
-                Gl.glVertex2f(x, y);
-            }
-            Gl.glEnd();
-            Gl.glFlush();
+            MoverCarrinho(key);
+            Glut.glutPostRedisplay();
         }
 
-        static void detalhesPista()
+        static void MoverCarrinho(int key)
         {
-            Gl.glColor3f(1.0f, 1.0f, 1.0f);
-
-            for(float i = 0.25f; i <= 0.85; i+= 0.05f)
+            switch (key)
             {
-                Gl.glBegin(Gl.GL_QUADS);
-                    Gl.glVertex2f(i - 0.05f, 0.19f);
-                    Gl.glVertex2f(i - 0.05f, 0.2f);
-                    Gl.glVertex2f(i - 0.02f, 0.2f);
-                    Gl.glVertex2f(i - 0.02f, 0.19f);
-                Gl.glEnd();
-                Gl.glFlush();
+                case Glut.GLUT_KEY_UP:
+                    ty += 0.03f;
+                    break;
+                case Glut.GLUT_KEY_DOWN:
+                    ty -= 0.03f;
+                    break;
+                case Glut.GLUT_KEY_LEFT:
+                    tx -= 0.03f;
+                    break;
+                case Glut.GLUT_KEY_RIGHT:
+                    tx += 0.03f;
+                    break;
             }
-
-            for (float i = 0.25f; i <= 0.85; i += 0.05f)
-            {
-                Gl.glBegin(Gl.GL_QUADS);
-                    Gl.glVertex2f(i - 0.05f, 0.79f);
-                    Gl.glVertex2f(i - 0.05f, 0.8f);
-                    Gl.glVertex2f(i - 0.02f, 0.8f);
-                    Gl.glVertex2f(i - 0.02f, 0.79f);
-                Gl.glEnd();
-                Gl.glFlush();
-            }
-
-            for (float i = 0.2f; i <= 0.75; i += 0.05f)
-            {
-                Gl.glBegin(Gl.GL_QUADS);
-                    Gl.glVertex2f(0.87f, i);
-                    Gl.glVertex2f(0.87f, i + 0.05f);
-                    Gl.glVertex2f(0.875f, i + 0.05f);
-                    Gl.glVertex2f(0.875f, i);
-                i += 0.05f;
-                Gl.glEnd();
-                Gl.glFlush();
-            }
-
-            for (float i = 0.2f; i <= 0.75; i += 0.05f)
-            {
-                Gl.glBegin(Gl.GL_QUADS);
-                    Gl.glVertex2f(0.12f, i);
-                    Gl.glVertex2f(0.12f, i + 0.05f);
-                    Gl.glVertex2f(0.125f, i + 0.05f);
-                    Gl.glVertex2f(0.125f, i);
-                i += 0.05f;
-                Gl.glEnd();
-                Gl.glFlush();
-            }
-        }
-
-        static void pista()
-        {
-            Gl.glColor3f(0.5f, 0.5f, 0.5f);
-
-            Gl.glBegin(Gl.GL_QUADS);
-                Gl.glVertex2f(0.05f, 0.1f);
-                Gl.glVertex2f(0.05f, 0.3f);
-                Gl.glVertex2f(0.95f, 0.3f);
-                Gl.glVertex2f(0.95f, 0.1f);
-            Gl.glEnd();
-            Gl.glFlush();
-
-            Gl.glColor3f(0.5f, 0.5f, 0.5f);
-
-            Gl.glBegin(Gl.GL_QUADS);
-                Gl.glVertex2f(0.05f, 0.9f);
-                Gl.glVertex2f(0.05f, 0.7f);
-                Gl.glVertex2f(0.95f, 0.7f);
-                Gl.glVertex2f(0.95f, 0.9f);
-            Gl.glEnd();
-            Gl.glFlush();
-
-            Gl.glBegin(Gl.GL_QUADS);
-                Gl.glVertex2f(0.05f, 0.1f);
-                Gl.glVertex2f(0.05f, 0.7f);
-                Gl.glVertex2f(0.2f, 0.7f);
-                Gl.glVertex2f(0.2f, 0.1f);
-            Gl.glEnd();
-            Gl.glFlush();
-
-            Gl.glBegin(Gl.GL_QUADS);
-            Gl.glVertex2f(0.8f, 0.1f);
-            Gl.glVertex2f(0.8f, 0.7f);
-            Gl.glVertex2f(0.95f, 0.7f);
-            Gl.glVertex2f(0.95f, 0.1f);
-            Gl.glEnd();
-            Gl.glFlush();
         }
 
         static void fundo()
@@ -146,13 +69,162 @@ namespace ProjetoFinalCarrinho
             Gl.glColor3f(0.6f, 0.3f, 0.0f);
 
             Gl.glBegin(Gl.GL_QUADS);
-                Gl.glVertex2f(0.0f, 0.0f);
-                Gl.glVertex2f(0.0f, 1.0f);
-                Gl.glVertex2f(1.0f, 1.0f);
-                Gl.glVertex2f(1.0f, 0.0f);
+            Gl.glVertex2f(0.0f, 0.0f);
+            Gl.glVertex2f(0.0f, 1.0f);
+            Gl.glVertex2f(1.0f, 1.0f);
+            Gl.glVertex2f(1.0f, 0.0f);
             Gl.glEnd();
-            Gl.glFlush();
+
+            Gl.glColor3f(0.0f, 1.0f, 0.0f);
+            Gl.glBegin(Gl.GL_QUADS);
+            Gl.glVertex2f(0.2f, 0.2f);
+            Gl.glVertex2f(0.2f, 0.7f);
+            Gl.glVertex2f(0.8f, 0.7f);
+            Gl.glVertex2f(0.8f, 0.2f);
+            Gl.glEnd();
+
         }
 
+        static void pista()
+        {
+            Gl.glColor3f(0.5f, 0.5f, 0.5f);
+
+            Gl.glBegin(Gl.GL_QUADS);
+            Gl.glVertex2f(0.05f, 0.1f);
+            Gl.glVertex2f(0.05f, 0.3f);
+            Gl.glVertex2f(0.95f, 0.3f);
+            Gl.glVertex2f(0.95f, 0.1f);
+            Gl.glEnd();
+
+
+            Gl.glColor3f(0.5f, 0.5f, 0.5f);
+
+            Gl.glBegin(Gl.GL_QUADS);
+            Gl.glVertex2f(0.05f, 0.9f);
+            Gl.glVertex2f(0.05f, 0.7f);
+            Gl.glVertex2f(0.95f, 0.7f);
+            Gl.glVertex2f(0.95f, 0.9f);
+            Gl.glEnd();
+
+
+            Gl.glBegin(Gl.GL_QUADS);
+            Gl.glVertex2f(0.05f, 0.1f);
+            Gl.glVertex2f(0.05f, 0.7f);
+            Gl.glVertex2f(0.2f, 0.7f);
+            Gl.glVertex2f(0.2f, 0.1f);
+            Gl.glEnd();
+
+
+            Gl.glBegin(Gl.GL_QUADS);
+            Gl.glVertex2f(0.8f, 0.1f);
+            Gl.glVertex2f(0.8f, 0.7f);
+            Gl.glVertex2f(0.95f, 0.7f);
+            Gl.glVertex2f(0.95f, 0.1f);
+            Gl.glEnd();
+
+        }
+
+        static void detalhesPista()
+        {
+            Gl.glColor3f(1.0f, 1.0f, 1.0f);
+
+            for (float i = 0.25f; i <= 0.85; i += 0.05f)
+            {
+                Gl.glBegin(Gl.GL_QUADS);
+                Gl.glVertex2f(i - 0.05f, 0.19f);
+                Gl.glVertex2f(i - 0.05f, 0.2f);
+                Gl.glVertex2f(i - 0.02f, 0.2f);
+                Gl.glVertex2f(i - 0.02f, 0.19f);
+                Gl.glEnd();
+
+            }
+
+            for (float i = 0.25f; i <= 0.85; i += 0.05f)
+            {
+                Gl.glBegin(Gl.GL_QUADS);
+                Gl.glVertex2f(i - 0.05f, 0.79f);
+                Gl.glVertex2f(i - 0.05f, 0.8f);
+                Gl.glVertex2f(i - 0.02f, 0.8f);
+                Gl.glVertex2f(i - 0.02f, 0.79f);
+                Gl.glEnd();
+
+            }
+
+            for (float i = 0.2f; i <= 0.75; i += 0.05f)
+            {
+                Gl.glBegin(Gl.GL_QUADS);
+                Gl.glVertex2f(0.87f, i);
+                Gl.glVertex2f(0.87f, i + 0.05f);
+                Gl.glVertex2f(0.875f, i + 0.05f);
+                Gl.glVertex2f(0.875f, i);
+                i += 0.05f;
+                Gl.glEnd();
+
+            }
+
+            for (float i = 0.2f; i <= 0.75; i += 0.05f)
+            {
+                Gl.glBegin(Gl.GL_QUADS);
+                Gl.glVertex2f(0.12f, i);
+                Gl.glVertex2f(0.12f, i + 0.05f);
+                Gl.glVertex2f(0.125f, i + 0.05f);
+                Gl.glVertex2f(0.125f, i);
+                i += 0.05f;
+                Gl.glEnd();
+
+            }
+        }
+
+        static void desenhaCarrinho()
+        {
+            float raio, x, y, pontos;
+            raio = 0.017f;
+            pontos = (2 * PI) / 16;
+
+            Gl.glPushMatrix();
+            Gl.glTranslatef(tx, ty, 0.0f);
+
+            Gl.glBegin(Gl.GL_QUADS);
+            Gl.glColor3f(1.0f, 0.0f, 0.0f);
+            Gl.glVertex2f(-0.05f, -0.05f);
+            Gl.glVertex2f(0.0f, -0.05f);
+            Gl.glVertex2f(0.0f, 0.0f);
+            Gl.glVertex2f(-0.05f, 0.0f);
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_QUADS);
+            Gl.glColor3f(0.0f, 0.0f, 1.0f);
+            Gl.glVertex2f(-0.02f, -0.03f);
+            Gl.glVertex2f(0.0f, -0.03f);
+            Gl.glVertex2f(0.0f, 0.0f);
+            Gl.glVertex2f(-0.02f, 0.0f);
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_TRIANGLE_FAN);
+            Gl.glColor3f(0f, 0f, 0f);
+            Gl.glLineWidth(1);
+            Gl.glBegin(Gl.GL_TRIANGLE_FAN);
+            for (float angulo = 0.0f; angulo <= 2 * PI; angulo += pontos)
+            {
+                x = (float)(raio * Math.Cos(angulo) + 0.151);
+                y = (float)(raio * Math.Sin(angulo) + 0.15);
+                Gl.glVertex2f(x - 0.2f, y - 0.2f);
+            }
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_TRIANGLE_FAN);
+            Gl.glColor3f(0f, 0f, 0f);
+            Gl.glLineWidth(1);
+            Gl.glBegin(Gl.GL_TRIANGLE_FAN);
+            for (float angulo = 0.0f; angulo <= 2 * PI; angulo += pontos)
+            {
+                x = (float)(raio * Math.Cos(angulo) + 0.195);
+                y = (float)(raio * Math.Sin(angulo) + 0.15);
+                Gl.glVertex2f(x - 0.2f, y - 0.2f);
+            }
+            Gl.glEnd();
+
+            Gl.glPopMatrix();
+        }
     }
 }
